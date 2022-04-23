0a2413afb8c5e86ffb2f4c8b035072612203d30a
yowgf <alexthomasmol@gmail.com>
2022-04-23 18:21:17 -0300

    Adjust to asgn. statement

--- lib/main/pool/pool.cpp
+++ lib/main/pool/pool.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+
 #include <pool/pool.hpp>
+#include <pool/process.hpp>
 #include <utils/log.hpp>
 
 namespace pool {
@@ -12,7 +15,36 @@ pool::pool(const utils::parsed_args& config)
 void pool::init()
 {}
 
+std::unique_ptr<task_descr_t> fetch_task(std::istream& is)
+{
+  int pid = 0;
+  int ms = 0;
+  is >> pid >> ms;
+  return std::make_unique<task_descr_t>(pid, ms);
+}
+
+bool is_eow(task_descr_t* td) {
+  return td->pid == 0 && td->ms == 0;
+}
+
+// TODOs:
+//
+// - master thread initializes the run, and worker threads execute the rest
+//
+// - pthread_exit instead of return
+//
 void pool::run()
-{}
+{
+  while (true) {
+    auto task = fetch_task(std::cin);
+    if (is_eow(task.get())) {
+      return;
+    }
+
+    processa(task.get());
+
+    return;
+  }
+}
 
 }
--- lib/main/utils/parse_args.cpp
+++ lib/main/utils/parse_args.cpp
@@ -5,8 +5,15 @@ namespace utils {
 parsed_args parse_args(int argc, char** argv)
 {
   std::string err_msg;
-
   bool debug = false;
+
+  constexpr int min_args = 3;
+  if (argc < min_args) {
+    err_msg = std::string("need at least ") + std::to_string(min_args-1) +
+      " arguments.";
+    return parsed_args{std::move(err_msg), debug};
+  }
+
   for (auto i = 1; i < argc; ++i) {
     if (std::string(argv[i]) == "-d") {
       debug = true;
